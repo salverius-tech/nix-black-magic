0d4aa093410ebf9f0c2a62fcab469f86c3b4faee
mike <219478+ilude@users.noreply.github.com>
2024-05-02 19:51:55 -0400

    oh ya

--- configuration.nix
+++ configuration.nix
@@ -1,25 +1,58 @@
 { config, lib, pkgs, ... }: 
 {
+  system.stateVersion = "23.11";
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  users.users.ilude = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ];
-    password = "";
-  };
-  
-  services.getty.autologinUser = "ilude";
-
-  nixpkgs.config.allowUnfree = true;
-
-	security.sudo.wheelNeedsPassword = false;
-
-	# disable ipv6
+  # disable ipv6
 	networking.enableIPv6  = false;
 
-  environment.systemPackages = with pkgs; [
-    python310
-  ];
+  #nixpkgs.config.allowUnfree = true;
 
-  system.stateVersion = "23.11";
+  users = {
+    defaultUserShell = pkgs.zsh;
+    users.ilude = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" "docker" "users" ];
+      shell = pkgs.zsh;
+      password = "";
+    };
+  };
+
+  services.getty.autologinUser = "ilude";
+
+  system.userActivationScripts = {
+    createZshrcFile = {
+      text = ''
+        if [ ! -f "$HOME/.zshrc" ]; then
+          touch "$HOME/.zshrc"
+        fi
+      '';
+    };
+  };
+
+  programs = {
+		# needed for vscode remote ssh
+		nix-ld.enable = true; 
+	 	zsh = {
+			enable = true;
+			autosuggestions.enable = true;
+			zsh-autoenv.enable = false;
+			syntaxHighlighting.enable = true;
+	  };
+  };
+  
+  security.sudo.wheelNeedsPassword = false;
+
+  environment = {
+    shells = [ pkgs.zsh pkgs.bash ];
+    shellAliases = {
+      l       = "ls -lhA --color=auto --group-directories-first";
+      nix-gc  = "nix-store --gc";
+      nix-rs  = "sudo nixos-rebuild switch";
+      sdn     = "sudo shutdown -h now";
+    };
+    systemPackages = with pkgs; [
+      python310
+    ];
+  };
 }
